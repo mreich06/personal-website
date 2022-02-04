42312aaab159e158bdacd6b345361f1cd9cb9d59
Maya Reich <mayareich0606@gmail.com>
2022-02-03 18:58:07 -0800

    background svg and about me section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import IntroCard from "./components/IntroCard";
+import Svg from "./assets/website-background.svg";
 import "./App.scss";
+import AboutMe from "./components/AboutMe";
 
 const App = (): JSX.Element => {
   return (
     <div className="appContainer">
       <IntroCard />
+      <img src={Svg} className="svgContainer" />
+      <AboutMe />
     </div>
   );
 };
---
+++ src/assets/TerminalIconSvg.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TerminalIconSvg = () => {
+  return <></>;
+};
+
+export default TerminalIconSvg;
